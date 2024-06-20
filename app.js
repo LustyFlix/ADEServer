a73bcce990b5d5ed46569b9ee5313ab284785211
LustyFlix <111301802+LustyFlix@users.noreply.github.com>
2024-06-20 12:15:58 +0600

    Update app.js

--- app.js
+++ app.js
@@ -176,10 +176,11 @@ app.get('/movie/:id/credits', (req, res) => {
             $('.movie-page__content-tags__performers a').each((index, element) => {
                 const href = $(element).attr('href');
                 const name = $(element).text().trim();
+                const character = $(element).text().trim();
                 const performerId = href ? href.split('/')[1] : '';
                 const poster_path = performerId ? `https://imgs1cdn.adultempire.com/actors/${performerId}h.jpg` : '';
                 
-                cast.push({ id: performerId, name, poster_path });
+                cast.push({ id: performerId, name, poster_path, character });
             });
     
             const crew = [];
@@ -190,7 +191,7 @@ app.get('/movie/:id/credits', (req, res) => {
                 const poster_path = crewId ? `https://imgs1cdn.adultempire.com/studio/${crewId}.jpg` : '';
     
                 if (crewId) {
-                    crew.push({ id: crewId, name, poster_path, "known_for_department": "Directing" });
+                    crew.push({ id: crewId, name, poster_path, "department": "Directing" });
                 }            
             });
     
